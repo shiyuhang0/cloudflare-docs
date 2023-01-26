b3340a67b1543746fb026b01505544d511ee5a17
Kody Jackson <kody@cloudflare.com>
2023-01-26 12:30:19 -0700

    manual test

--- assets/sparrow.ts
+++ assets/sparrow.ts
@@ -1,27 +1,61 @@
 import sparrow from '@cloudflare/util-sparrow';
 
+// testing with hard-keyed values
+
+const userId = "c5f71bb97fb2f00b17f20a5d97860fda"
+const deviceId = "f76bd726-b493-49d3-bb98-9348747821cf"
+
 // These keys are fine to be exposed publicly 
-let stagingKey = "f2c3f386e64d4e89bcbe32f273aa83eb"
-let productionKey = "ab1cea39a1d94652aa2a2ce1f42275ae"
+const stagingKey = "f2c3f386e64d4e89bcbe32f273aa83eb"
+const productionKey = "ab1cea39a1d94652aa2a2ce1f42275ae"
 let keyToUse;
 
 // Based on current environment, send events to the correct location
-let hostname = window.location.hostname
+const hostname = window.location.hostname
 
-if (hostname === "developers.cloudflare.com") {
+/* if (hostname === "developers.cloudflare.com") {
   keyToUse = productionKey;
 } else {
   keyToUse = stagingKey
+} */
+
+declare const SPARROW_SOURCE_KEY: string;
+const SPARROW_URL = "https://sparrow.cloudflare.com";
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
+
 
 export function init() {
 
 sparrow.init({
-    sourceKey: keyToUse
+    sourceKey: productionKey
 });
 
-sparrow.sendEvent("pageview", {
+sparrow.setUserId(userId);
+
+sparrow.identify({
+  deviceId: deviceId,
+	userId: userId
+});
+
+sparrow.sendEvent("documentation pageview", {
     page: window.location.pathname,
   });
+
+  console.log(getCookie("sparrow_id"));
 
 }
